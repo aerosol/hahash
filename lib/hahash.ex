1c0bcb4ca575ecda262b8ad94fc2ac6157fd8e32
Adam Rutkowski <hq@mtod.org>
2023-08-05 23:00:51 +0200

    Update docs

--- lib/hahash.ex
+++ lib/hahash.ex
@@ -1,14 +1,19 @@
 defmodule Hahash do
   @moduledoc """
   Hash any term into a cute name.
+  There is no guarantees all will end up cute.
+
+  Just in case, don't use it for anything very serious.
+  I'm happy to remove words, if you feel like curating the list.
   """
   use Hahash.Wordlist
 
   @spec name(any()) :: String.t()
   @doc """
-  Proivide any term (atom, map, whatever), get a name back.
-  The names consist of verb, adjective and a noun, separated
-  with hyphen.
+  Provide any term (atom, map, whatever), get a name back.
+
+  The name is a string, consisting of a triple: 
+  verb, adjective and a noun, separated with hyphen.
   """
   def name(term) do
     verb_index = :erlang.phash2(term, verbs_size())
--- lib/hahash/wordlist.ex
+++ lib/hahash/wordlist.ex
@@ -1,4 +1,11 @@
 defmodule Hahash.Wordlist do
+  @moduledoc """
+  Words. 
+
+  Source material: https://github.com/imsky/wordlists
+
+  Raw files are stored in `priv/` directory.
+  """
   defmacro __using__(_opts) do
     quote do
       require Hahash.Wordlist
@@ -22,12 +29,21 @@ defmodule Hahash.Wordlist do
         acc ++ words
       end)
 
+    @doc """
+    Returns the size of #{category} collection.
+    Can be used in guards.
+    """
     defmacro unquote(:"#{category}_size")() do
-      quote bind_quoted: [words: unquote(words)] do
-        length(words)
+      quote bind_quoted: [size: unquote(length(words))] do
+        size
       end
     end
 
+    @doc """
+    Given an index, greater or equal to 1,
+    and less or equal than #{length(words)},
+    returns a word from '#{category}' category.
+    """
     for {word, index} <- Enum.with_index(words, 1) do
       def unquote(String.to_atom(category))(unquote(index)), do: unquote(word)
     end
