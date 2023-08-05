388f4d6788261e77a8db249123c75a704a5fc566
Adam Rutkowski <hq@mtod.org>
2023-08-05 22:52:10 +0200

    Update moduledocs

--- lib/hahash.ex
+++ lib/hahash.ex
@@ -1,7 +1,15 @@
 defmodule Hahash do
+  @moduledoc """
+  Hash any term into a cute name.
+  """
   use Hahash.Wordlist
 
   @spec name(any()) :: String.t()
+  @doc """
+  Proivide any term (atom, map, whatever), get a name back.
+  The names consist of verb, adjective and a noun, separated
+  with hyphen.
+  """
   def name(term) do
     verb_index = :erlang.phash2(term, verbs_size())
     adjective_index = :erlang.phash2(term, adjectives_size())
